e39811caa7e23412fda1a03079a110b7fa82d1cc
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-11-20 11:55:22 +0300

    Add files via upload

---
+++ arama örneği-847/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arama_örneği_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+          
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            bool durum = false;
+            int sayac = 0;
+
+            for ( int i = 0; i < lbAdlar.Items.Count; i++)
+            {
+                string aranan = lbAdlar.Items[i].ToString();
+                if(aranan == txtAranan.Text)
+                {
+                    durum = true;
+                    sayac++; // aranan kişiyi bir arttır
+                }
+            }
+            // döngü bittikten sonra
+            if(durum)
+            {
+                lblSonuc.Text = "aradığınız kişiden " + sayac + "tane var.";
+            }
+            else
+            {
+                lblSonuc.Text = "aradığınız kişi yok.";
+            }
+        }
+    }
+}
